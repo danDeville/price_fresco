ec36b5ed6fe51cc6b71e243ebdf91926f1a4e140
dandeville <dandeville26@gmail.com>
2022-04-08 16:33:29 -0500

    Consumo de API y creación de grilla de productos

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,34 @@
-console.log('Hello World');
+const API = 'https://backend-principe-fresco.herokuapp.com/camisetas/'
+
+const listarCards = document.getElementById('listarCards')
+
+const getApi = async(url) => {
+  const listarApi  = await axios.get(url)
+  const { data } = listarApi
+
+  listarCards.innerHTML = ''
+  data.forEach(e => {
+    const { id,titulo, precio, imagenFront } = e
+    listarCards.innerHTML += `
+    <div class="col s12 m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="${imagenFront}" alt="${titulo}" style="height:430px; object-fit: cover;">
+        </div>
+        <div class="card-content center-align">
+          <span class="card-title">${titulo}</span>
+          <p>${precio}</p>
+          <buutton class="btn black" style="margin-top: 12px">Ver Detalle</buutton>
+        </div>
+      </div>
+    </div>
+    `
+  })
+}
+
+getApi(API)
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+})
